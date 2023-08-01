dc7b25d4600e3791bd04f499cbd2930c287b6ff7
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-01 14:24:48 +0530

    ....

---
+++ React-Progs/myapp/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import Card from './Card';
+function App() {
+  return (
+    <>
+      <div className="container d-flex">
+        <Card title = "dev" />
+        <Card title = "tirth"/>
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
