8942df3cc00931766b0d18c9fb2b22c5e27feff1
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-02 16:29:06 +0530

    ...

--- My-Practice Progs/second-app/src/App.js
+++ My-Practice Progs/second-app/src/App.js
@@ -1,29 +1,15 @@
 import Navbar from './Navbar';
 import Card from './Card';
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios.get('https://api.slingacademy.com/v1/sample-data/photos')
-      .then(response => {
-        setData(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+ 
   return (
     <>
       <div>
         <Navbar />
-        <div className='d-flex container'>
-          {data.map(item => (
-            <Card  /> 
-          ))}
-        </div>
+          <Card />
       </div>
     </>
   );
