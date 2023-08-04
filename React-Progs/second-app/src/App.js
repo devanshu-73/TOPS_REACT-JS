be45f68dd511568be05abed6f94ad6896a82118f
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-04 16:53:44 +0530

    ..

---
+++ React-Progs/second-app/src/App.js
@@ -0,0 +1,18 @@
+import Navbar from './Navbar';
+import Card from './Card';
+import './App.css';
+
+
+function App() {
+ 
+  return (
+    <>
+      <div>
+        <Navbar />
+          <Card />
+      </div>
+    </>
+  );
+}
+
+export default App;
