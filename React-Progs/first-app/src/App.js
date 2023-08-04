0ce633f69008d0fcc49df831dbcdb2ea675402de
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-04 16:52:20 +0530

    .....

---
+++ React-Progs/first-app/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import NavScrollExample from "./MainNavbar";
+import TextExample from "./CardMain";
+function App() {
+  return (
+    <>
+      <NavScrollExample />
+      <div className="d-flex">
+        <TextExample />
+        <TextExample />
+        <TextExample />
+      </div>
+    </>
+  );
+}
+
+export default App;
