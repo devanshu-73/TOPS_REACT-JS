e8d199d33ed7566ad345cfba9aa0dff9c6abd479
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-07 19:06:41 +0530

    ..

--- React-Progs/myapp/src/App.js
+++ React-Progs/myapp/src/App.js
@@ -2,16 +2,19 @@
 import './App.css';
 import Card from './Card';
 function App() {
-  let title = ["ABC", "dev", "Jay", "tirth", "XYZ"]
+  let titleArr = ["ABC", "dev", "Jay", "tirth", "XYZ"]
   return (
     <>
       <div className="container d-flex">
+      
         {/* <Card title="ABC" />
         <Card title="dev" />
         <Card title="Jay" />
         <Card title="tirth" />
         <Card title="XYZ" /> */}
-        <Card title="ABC" />
+        {titleArr.map((val) => (
+          <Card title={val} />
+        ))}
 
       </div>
     </>
