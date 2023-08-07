c2f9676f434441d6f8d06ce95f7dcc17ec9237f9
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-07 19:04:09 +0530

    ..

--- React-Progs/myapp/src/App.js
+++ React-Progs/myapp/src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import Card from './Card';
 function App() {
+  let title = ["ABC", "dev", "Jay", "tirth", "XYZ"]
   return (
     <>
       <div className="container d-flex">
-        <Card title="ABC" />
+        {/* <Card title="ABC" />
         <Card title="dev" />
         <Card title="Jay" />
         <Card title="tirth" />
-        <Card title="XYZ" />
+        <Card title="XYZ" /> */}
+        <Card title="ABC" />
+
       </div>
     </>
   );
