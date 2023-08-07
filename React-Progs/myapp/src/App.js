65537eb24f248816249dcd325217f5d0e3cc6168
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-07 19:08:58 +0530

    ..

--- React-Progs/myapp/src/App.js
+++ React-Progs/myapp/src/App.js
@@ -5,13 +5,17 @@ function App() {
   let titleArr = ["ABC", "dev", "Jay", "tirth", "XYZ"]
   return (
     <>
+      {/* -------------------------------------------------------------------------- */}
       <div className="container d-flex">
-      {/* Hard coding... */}
+        {/* Hard coding... */}
         {/* <Card title="ABC" />
         <Card title="dev" />
         <Card title="Jay" />
         <Card title="tirth" />
         <Card title="XYZ" /> */}
+
+        {/* -------------------------------------------------------------------------- */}
+        {/* Smart Coding */}
         {titleArr.map((val) => (
           <Card title={val} />
         ))}
