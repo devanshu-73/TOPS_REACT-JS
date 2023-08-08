09008786c1a17f5125375ccdc70b2a96145138d4
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-08 14:06:33 +0530

    ..

--- React-Progs/vite-first-app/src/Card.jsx
+++ React-Progs/vite-first-app/src/Card.jsx
@@ -1,11 +1,11 @@
 
 // eslint-disable-next-line react/prop-types
-export default function Card({ title }) {
+export default function Card({ title, img }) {
     return (
         <>
-            <div className="container row">
-                <div className="card col-3" style={{ width: 400 }}>
-                    <img className="card-img-top" src="../bootstrap4/img_avatar1.png" alt="Card image" />
+            <div className="col-3">
+                <div className="card " style={{ width: "100%" }}>
+                    <img className="card-img-top" src={img} alt="Card image" style={{ height:'100'}} />
                     <div className="card-body">
                         <h4 className="card-title">{title}</h4>
                         <p className="card-text">Some example text some example text. John Doe is an architect and engineer</p>
