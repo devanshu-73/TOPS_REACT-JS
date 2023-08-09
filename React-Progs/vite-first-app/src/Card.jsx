efb60c0135333b8bc02b141a4f7b3223cac35025
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-09 19:24:34 +0530

    ..

--- React-Progs/vite-first-app/src/Card.jsx
+++ React-Progs/vite-first-app/src/Card.jsx
@@ -8,7 +8,7 @@ export default function Card({ title, img }) {
                     <img className="card-img-top" src={img} alt="Card image" style={{ height:'100'}} />
                     <div className="card-body">
                         <h4 className="card-title">{title}</h4>
-                        <p className="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                        <p className="card-text">Some example text some example text. John Doe is </p>
                         <a href="#" className="btn btn-primary">See Profile</a>
                     </div>
                 </div>
