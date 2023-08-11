71d573a14c5244d64c4d0e7b4e57efd3aef4fbff
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-11 19:27:34 +0530

    ...

--- React-Progs/routing-project/src/Component/About.jsx
+++ React-Progs/routing-project/src/Component/About.jsx
@@ -8,6 +8,7 @@ export default function About() {
                 <p>Some text..</p>
                 <p>Some text..</p>
                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
             </div>
         </>
     )
