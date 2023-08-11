f8cd72ab4805a0bb82a8574d9a723e6af141438c
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-11 19:28:11 +0530

    ....

--- React-Progs/routing-project/src/Component/Footer.jsx
+++ React-Progs/routing-project/src/Component/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     return (
         <div>
             <div className=" mt-5 p-4 bg-dark text-white text-center">
-                <p>Footer</p>
+                <p>Website Footer</p>
             </div>
         </div>
     )
