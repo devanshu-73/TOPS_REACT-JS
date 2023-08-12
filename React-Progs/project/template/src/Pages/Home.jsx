47a91655ab0b176898cdd35f8e0903a3a4277e46
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-12 23:16:48 +0530

    ...

--- React-Progs/project/template/src/Pages/Home.jsx
+++ React-Progs/project/template/src/Pages/Home.jsx
@@ -129,13 +129,13 @@ export default function Home() {
                         <div className="col-lg-6">
                             <div className="row g-3">
                                 <div className="col-6 text-end">
-                                    <img className="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" style={{ marginTop: '25%' }} />
+                                    <img className="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src={'img/about-1.jpg'} style={{ marginTop: '25%' }} />
                                 </div>
                                 <div className="col-6 text-start">
-                                    <img className="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg" />
+                                    <img className="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s" src={'img/about-2.jpg'} />
                                 </div>
                                 <div className="col-6 text-end">
-                                    <img className="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg" />
+                                    <img className="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s" src={'img/about-3.jpg'} />
                                 </div>
                                 <div className="col-6 text-start">
                                     <img className="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg" />
@@ -407,6 +407,5 @@ export default function Home() {
             {/* Testimonial End */}
 
         </div>
-
-    )
+    );
 }
