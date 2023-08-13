32f468d355de6650129d6ac6e5cf0dfd991b8eb4
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-13 21:31:05 +0530

    ...

--- React-Progs/project/template/src/Pages/Booking.jsx
+++ React-Progs/project/template/src/Pages/Booking.jsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 export default function Booking() {
     return (
-        <div>
+        <>
             {/* Page Header Start */}
-            <div className="container-fluid page-header mb-5 p-0" style={{ backgroundImage: `url(img/carousel-1.jpg)` }}>
+            <div className="container-fluid page-header mb-5 p-0" style={{ backgroundImage: 'url(img/carousel-1.jpg)' }}>
                 <div className="container-fluid page-header-inner py-5">
                     <div className="container text-center pb-5">
-                        <h1 className="display-3 text-white mb-3 animated slideInDown">Booking</h1>
+                        <h1 className="display-3 text-white mb-3 animated slideInDown">About Us</h1>
                         <nav aria-label="breadcrumb">
                             <ol className="breadcrumb justify-content-center text-uppercase">
                                 <li className="breadcrumb-item"><a href="#">Home</a></li>
                                 <li className="breadcrumb-item"><a href="#">Pages</a></li>
-                                <li className="breadcrumb-item text-white active" aria-current="page">Booking</li>
+                                <li className="breadcrumb-item text-white active" aria-current="page">About</li>
                             </ol>
                         </nav>
                     </div>
@@ -163,6 +163,6 @@ export default function Booking() {
                 </div>
             </div>
             {/* Booking End */}
-        </div>
+        </>
     );
 }
