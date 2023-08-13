3ecb1e8b0e14bd284384b2fffb430a393ff8f030
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-13 21:30:47 +0530

    ..

--- React-Progs/project/template/src/Pages/About.jsx
+++ React-Progs/project/template/src/Pages/About.jsx
@@ -4,71 +4,66 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-
-
-            <div>
-                {/* Page Header Start */}
-                <div className="container-fluid page-header mb-5 p-0" style={{ backgroundImage: 'url(img/carousel-1.jpg)' }}>
-                    <div className="container-fluid page-header-inner py-5">
-                        <div className="container text-center pb-5">
-                            <h1 className="display-3 text-white mb-3 animated slideInDown">About Us</h1>
-                            <nav aria-label="breadcrumb">
-                                <ol className="breadcrumb justify-content-center text-uppercase">
-                                    <li className="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li className="breadcrumb-item"><a href="#">Pages</a></li>
-                                    <li className="breadcrumb-item text-white active" aria-current="page">About</li>
-                                </ol>
-                            </nav>
-                        </div>
+        <>
+            {/* Page Header Start */}
+            <div className="container-fluid page-header mb-5 p-0" style={{ backgroundImage: 'url(img/carousel-1.jpg)' }}>
+                <div className="container-fluid page-header-inner py-5">
+                    <div className="container text-center pb-5">
+                        <h1 className="display-3 text-white mb-3 animated slideInDown">About Us</h1>
+                        <nav aria-label="breadcrumb">
+                            <ol className="breadcrumb justify-content-center text-uppercase">
+                                <li className="breadcrumb-item"><a href="#">Home</a></li>
+                                <li className="breadcrumb-item"><a href="#">Pages</a></li>
+                                <li className="breadcrumb-item text-white active" aria-current="page">About</li>
+                            </ol>
+                        </nav>
                     </div>
                 </div>
-                {/* Page Header End */}
-                {/* Booking Start */}
-                <div className="container-fluid booking pb-5 wow fadeIn" data-wow-delay="0.1s">
-                    <div className="container">
-                        <div className="bg-white shadow" style={{ padding: 35 }}>
-                            <div className="row g-2">
-                                <div className="col-md-10">
-                                    <div className="row g-2">
-                                        <div className="col-md-3">
-                                            <div className="date" id="date1" data-target-input="nearest">
-                                                <input type="text" className="form-control datetimepicker-input" placeholder="Check in" data-target="#date1" data-toggle="datetimepicker" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-3">
-                                            <div className="date" id="date2" data-target-input="nearest">
-                                                <input type="text" className="form-control datetimepicker-input" placeholder="Check out" data-target="#date2" data-toggle="datetimepicker" />
-                                            </div>
-                                        </div>
-                                        <div className="col-md-3">
-                                            <select className="form-select">
-                                                <option selected>Adult</option>
-                                                <option value={1}>Adult 1</option>
-                                                <option value={2}>Adult 2</option>
-                                                <option value={3}>Adult 3</option>
-                                            </select>
-                                        </div>
-                                        <div className="col-md-3">
-                                            <select className="form-select">
-                                                <option selected>Child</option>
-                                                <option value={1}>Child 1</option>
-                                                <option value={2}>Child 2</option>
-                                                <option value={3}>Child 3</option>
-                                            </select>
+            </div>
+            {/* Page Header End */}
+            {/* Booking Start */}
+            <div className="container-fluid booking pb-5 wow fadeIn" data-wow-delay="0.1s">
+                <div className="container">
+                    <div className="bg-white shadow" style={{ padding: 35 }}>
+                        <div className="row g-2">
+                            <div className="col-md-10">
+                                <div className="row g-2">
+                                    <div className="col-md-3">
+                                        <div className="date" id="date1" data-target-input="nearest">
+                                            <input type="text" className="form-control datetimepicker-input" placeholder="Check in" data-target="#date1" data-toggle="datetimepicker" />
                                         </div>
                                     </div>
+                                    <div className="col-md-3">
+                                        <div className="date" id="date2" data-target-input="nearest">
+                                            <input type="text" className="form-control datetimepicker-input" placeholder="Check out" data-target="#date2" data-toggle="datetimepicker" />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <select className="form-select">
+                                            <option selected>Adult</option>
+                                            <option value={1}>Adult 1</option>
+                                            <option value={2}>Adult 2</option>
+                                            <option value={3}>Adult 3</option>
+                                        </select>
+                                    </div>
+                                    <div className="col-md-3">
+                                        <select className="form-select">
+                                            <option selected>Child</option>
+                                            <option value={1}>Child 1</option>
+                                            <option value={2}>Child 2</option>
+                                            <option value={3}>Child 3</option>
+                                        </select>
+                                    </div>
                                 </div>
-                                <div className="col-md-2">
-                                    <button className="btn btn-primary w-100">Submit</button>
-                                </div>
+                            </div>
+                            <div className="col-md-2">
+                                <button className="btn btn-primary w-100">Submit</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                {/* Booking End */}
             </div>
-
+            {/* Booking End */}
             {/* About Start */}
             <div className="container-xxl py-5">
                 <div className="container">
@@ -128,7 +123,7 @@ export default function About() {
                 </div>
             </div>
             {/* About End */}
-        </div>
+        </>
 
     )
 }
