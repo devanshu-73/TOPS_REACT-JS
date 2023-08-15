508fb4f24723a05e683fc9e5503cfef5563576ce
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-15 22:11:11 +0530

    ..

--- React-Progs/project/admin-panel/src/App.js
+++ React-Progs/project/admin-panel/src/App.js
@@ -1,29 +1,25 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Compo/Header";
-import Footer from './Compo/Footer';
-import Sidebar from './Compo/Sidebar';
-import Main from './Pages/Main';
-// import Index2 from './Pages/Index2';
+import Header from "./Comp/Header";
+import Footer from "./Comp/Footer";
+import Main from "./Comp/Main";
+// import Sidebar from "./Comp/Sidebar";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<>
-          <Header />
-          <Sidebar /><Main />
-          <Footer />
-        </>}></Route>
-
-        {/* <Route exact path="/index2" element={<>
-          <Header />
-          <Index2 />
-          <Footer />
-        </>}></Route> */}
-
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Header />
+              <Main />
+              <Footer />
+            </>
+          }
+        ></Route>
       </Routes>
-    </BrowserRouter >
-
+    </BrowserRouter>
   );
 }
 
