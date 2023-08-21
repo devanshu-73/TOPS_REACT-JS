8c4325c2c0ce8b560d1cd060058ced772362811a
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-21 18:48:26 +0530

    ...

--- My-Practice Progs/project/admin-dash-1/src/App.jsx
+++ My-Practice Progs/project/admin-dash-1/src/App.jsx
@@ -1,14 +1,13 @@
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Comp/Header';
 function App() {
   return (
     <>
-    <Header />  
-      {/* <BrowserRouter>
+      <BrowserRouter>
       <Routes>
         <Route path='/' element={<><Header /></>}> </Route>
       </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </>
   )
 }
