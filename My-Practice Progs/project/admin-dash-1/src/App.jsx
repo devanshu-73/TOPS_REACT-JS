54bfa41f896dcdf238fc34f6bf83b81311eab55e
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-21 19:22:44 +0530

    ..

--- My-Practice Progs/project/admin-dash-1/src/App.jsx
+++ My-Practice Progs/project/admin-dash-1/src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Sidebar from './Comp/Sidebar';
-import Home from './Comp/Home';
+// import Home from './Comp/Home';
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<><Sidebar /><Home /></>}> </Route>
+          <Route path='/' element={<><Sidebar /></>}> </Route>
         </Routes>
       </BrowserRouter>
     </>
