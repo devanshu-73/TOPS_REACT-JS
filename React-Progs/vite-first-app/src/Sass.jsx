0e0ddf60907a423fa96bea7478e2c8daf866d1b7
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-22 17:13:56 +0530

    ....

---
+++ React-Progs/vite-first-app/src/Sass.jsx
@@ -0,0 +1,27 @@
+import './Style.scss';
+export default function Sass() {
+    return (
+        <div>
+            <h1 className="dev">Hello</h1>
+            <div>    <nav>
+                <ul>
+                    <li><a href="">efef</a></li>
+                    <li><a href="">hth</a></li>
+                    <li><a href="">hyh</a></li>
+                    <li><a href="">gere</a></li>
+                </ul>
+            </nav></div>
+            <div className="myfont">Hello How r u?</div>
+
+            {/* // 4)Extend(inheritence) in sass */}
+
+            <div>
+                <button className='btn-1'>Submit</button>
+                <button className='btn-2'>Submit</button>
+                <button className='btn-3'>Submit</button>
+                <button className='btn-4'>Submit</button>
+            </div>
+        </div>
+    )
+}
+
