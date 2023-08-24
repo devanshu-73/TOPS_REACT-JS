b0f1566d0d293d62be94d5148e00c3a0fd5b7f32
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-24 19:32:36 +0530

    ...

--- React-Progs/vite-first-app/src/MUI/Button.jsx
+++ React-Progs/vite-first-app/src/MUI/Button.jsx
@@ -1,19 +1,16 @@
-import Button from "@mui/material/Button";
-
+// import * as MUI from '@material-ui/core';
 const ButtonMui = () => {
+ 
+   
     return (
-        <div className="container">
-            <Button variant="text" color="primary">
-                Dev1
-            </Button>
-            <Button variant="contained" color="secondary">
-                Dev2
-            </Button>
-            <Button variant="disabled" color="danger">
-                Dev3
-            </Button>
-        </div>
-    );
-};
+        <div className='container'>
+        
 
-export default ButtonMui;
+        
+        </div>
+
+
+    )
+}
+
+export default ButtonMui
