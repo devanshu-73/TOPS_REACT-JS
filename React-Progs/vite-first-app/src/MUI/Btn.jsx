de12cebec7f456d2cf937a11acc30eb4407b7d76
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-25 18:46:59 +0530

    ...

---
+++ React-Progs/vite-first-app/src/MUI/Btn.jsx
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button'
+
+export default function Btn() {
+  return (
+    <div>wkkencekn
+      <Button variant="text" color="primary">
+        Btn-Primary
+      </Button>
+    </div>
+  )
+}
