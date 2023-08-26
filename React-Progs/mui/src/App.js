554a2e7e803eeaaf6f2f3e27d170796280aa0b07
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-26 21:11:08 +0530

    ...

--- React-Progs/mui/src/App.js
+++ React-Progs/mui/src/App.js
@@ -1,8 +1,10 @@
 import Button from '@mui/material/Button';
 // import StyledComp from './Styled_Comp';
 function App() {
-  let text1 = ["hello", "frew", "rv", "btrbr", "vrr"];
-  let color1 = ["yellow", "white", "red", "blur", "orange"];
+  // let text1 = ["hello", "frew", "rv", "btrbr", "vrr"];
+  let text1 = ["hello", "frew"];
+  let color1 = ["yellow", "white", "red"];
+  // let color1 = ["yellow", "white", "red", "blur", "orange"];
   return (
     <>
       <Button variant="contained" color="primary">
