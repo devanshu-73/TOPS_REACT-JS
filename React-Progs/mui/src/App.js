377e2462b6de26d002c87bbefe399e9bbeb69527
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-08-26 21:56:37 +0530

    ...

--- React-Progs/mui/src/App.js
+++ React-Progs/mui/src/App.js
@@ -1,17 +1,20 @@
 import Button from '@mui/material/Button';
-// import StyledComp from './Styled_Comp';
+import StyledComp from './Styled_Comp';
 function App() {
-  // let text1 = ["hello", "frew", "rv", "btrbr", "vrr"];
-  // let color1 = ["yellow", "white", "red", "blur", "orange"];
+  let text1 = ["hello", "frew", "rv", "btrbr", "vrr"];
+  let color1 = ["yellow", "white", "red", "blue", "orange"];
   return (
     <>
-      <Button variant="contained" color="primary">
-        Dev-1
-      </Button>
+      <Button variant="contained" color="primary">Dev-1</Button>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
-      <hr />
+      <hr /><hr /><hr />
+
+      {
+        text1.map((val, index) => (
+          <StyledComp key={index} text={val} color_1={color1[index]} />
+        ))
+      }
     </>
   );
 }
