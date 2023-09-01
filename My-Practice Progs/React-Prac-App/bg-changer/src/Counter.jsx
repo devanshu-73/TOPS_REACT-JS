1cc969bfe5e9597c9e6fac034734f51f015c93b2
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-01 19:26:30 +0530

    ...

--- My-Practice Progs/React-Prac-App/bg-changer/src/Counter.jsx
+++ My-Practice Progs/React-Prac-App/bg-changer/src/Counter.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
+import React,{ useState } from "react"
 const Counter = () => {
+    const [state, setstate] = useState(0);
+    const add =()=>{
+        
+    }
   return (
     <div>
-      
+        <button onClick={minus} style={{marginRight: 10}}>-</button><span>{state}</span><button onClick={add} style={{marginLeft: 10}}>+</button>
     </div>
   )
 }
