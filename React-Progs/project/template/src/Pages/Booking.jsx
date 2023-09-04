6fbcbb2362c38ec823425b74b97d54dabed98a7e
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-04 18:56:23 +0530

    ...

--- React-Progs/project/template/src/Pages/Booking.jsx
+++ React-Progs/project/template/src/Pages/Booking.jsx
@@ -165,7 +165,7 @@ export default function Booking() {
                     </div>
                 </div>
             </div>
-            {/* Booking End */}
+            {/* Booking-End */}
 
         </>
     );
