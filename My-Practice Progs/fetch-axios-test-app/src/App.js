9332da36dece9de4a05de41037f1e26d3a327a64
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-07 19:44:15 +0530

    ...

--- My-Practice Progs/fetch-axios-test-app/src/App.js
+++ My-Practice Progs/fetch-axios-test-app/src/App.js
@@ -1,10 +1,10 @@
-import FetchApi from './FetchApi';
+// import FetchApi from './FetchApi';
 import AxiosApi from './AxiosApi';
 
 function App() {
   return (
     <div>
-      <FetchApi />
+      {/* <FetchApi /> */}
       <AxiosApi />
 
     </div>
