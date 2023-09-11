5d05986be938d4bfcd7ab6c42ce79512507bda62
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-11 19:32:37 +0530

    ..

--- React-Progs/vite-first-app/src/Sass.jsx
+++ React-Progs/vite-first-app/src/Sass.jsx
@@ -5,10 +5,10 @@ export default function Sass() {
             <h1 className="dev">Hello</h1>
             <div>    <nav>
                 <ul>
-                    <li><a href="">efef</a></li>
-                    <li><a href="">htgh</a></li>
-                    <li><a href="">hgyh</a></li>
-                    <li><a href="">gere</a></li>
+                    <li><a href="">One</a></li>
+                    <li><a href="">Two</a></li>
+                    <li><a href="">Three</a></li>
+                    <li><a href="">Four</a></li>
                 </ul>
             </nav></div>
             <div className="myfont">Hello How r u?</div>
