133df21d80ac0a909566e33f007aa30086d2196c
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-15 19:42:35 +0530

    ...

--- My-Practice Progs/testing-app/src/App.js
+++ My-Practice Progs/testing-app/src/App.js
@@ -1,13 +1,10 @@
-import { useState } from "react";
+
 
 function App() {
-  const [colors, setColors] = useState(['red', 'yellow', 'green', 'blue', 'black'])
   return (
 
     <>
-      <div width={100} style={{ backgroundColor: { colors } }} id="bgcolor">
-      </div>
-      <button onClick={colors.map(color => setColors(color))}>Click Me</button>
+     
     </>
   );
 }
