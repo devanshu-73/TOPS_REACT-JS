7838c60e73cf4d2a0d29648950551aed95ad1b3c
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-15 19:30:30 +0530

    ..

--- My-Practice Progs/testing-app/src/App.js
+++ My-Practice Progs/testing-app/src/App.js
@@ -6,9 +6,12 @@ function App() {
     colors.map(color => setColors(color))
   }
   return (
-    <div width={100}style={{backgroundColor:}} id="bgcolor">
-    </div>
+
+    <>
+      <div width={100} style={{ backgroundColor:}} id="bgcolor">
+      </div>
       <button onClick={colorChange}>Click Me</button>
+    </>
   );
 }
 
