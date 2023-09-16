2b7ca56be6a29317b0eb1755b2feabf2863b5d00
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-16 18:29:35 +0530

    ...

--- My-Practice Progs/testing-app/src/App.js
+++ My-Practice Progs/testing-app/src/App.js
@@ -1,17 +1,29 @@
-// import { useState } from "react";
+import { useState } from "react";
+
 function App() {
-  // const [color, setColor] = useState(['red', 'blue', 'green', 'black'])
+  const colors = ['red', 'blue', 'green', 'black'];
+  const [index, setIndex] = useState(-1); // Start with -1 for no color
+
   const bgChange = () => {
-    console.log("hello");
-    // const bgDiv = document.getElementById('bgDiv');
-    // for (let i = 0; i < color.length; i++) {
-    //   bgDiv.style.backgroundColor = setColor(color[i]);
-    // }
+    if (index === colors.length - 1) {
+      setIndex(0); // Reset to the first color when reaching the end
+    } else {
+      setIndex(index + 1); // Increment the index
+    }
   }
+
   return (
     <>
-      <div id="bgDiv" style={{ width: '100px', height: '100px', border: '1px solid black' }}></div>
-      <button onClick={() => (bgChange())}> click</button>
+      <div
+        id="bgDiv"
+        style={{
+          width: '100px',
+          height: '100px',
+          border: '1px solid black',
+          backgroundColor: index === -1 ? 'transparent' : colors[index], // Set transparent background for -1 index
+        }}
+      ></div>
+      <button onClick={bgChange}>Click</button>
     </>
   );
 }
