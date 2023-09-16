e600b8720fa0d0ff3549acc1f897e9a458587fe8
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-16 17:25:22 +0530

    ....

--- My-Practice Progs/bg-changer/src/App.js
+++ My-Practice Progs/bg-changer/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Count from './Count'
 // import Counter from './Counter'
+import Bgcolor from './Bgcolor';
 const App = () => {
   return (
     <div>
       <Count />
+      <Bgcolor />
       {/* <Counter /> */}
     </div>
   )
