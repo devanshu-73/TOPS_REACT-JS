2c7a818cab87ee68eff9441283f1afc6fdc4085c
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-21 19:47:34 +0530

    ...

---
+++ React-Progs/project/template/src/Pages/Login.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+
+function SignUp() {
+    return (
+        <div>
+            <div className="container" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 100 }}>
+                <div className="row " >
+                    <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                        <div className="panel-body">
+                            <form role="form" style={{ width: "300px", marginTop: 20, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                <h5>Enter Details to Login</h5>
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-tag" /></span>
+                                    <input type="text" className="form-control" placeholder="Your UserName " />
+                                </div>
+
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-lock" /></span>
+                                    <input type="password" className="form-control" placeholder="Your Password" />
+                                </div>
+                                <a href="index.html" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className="btn btn-primary ">Login Now</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
