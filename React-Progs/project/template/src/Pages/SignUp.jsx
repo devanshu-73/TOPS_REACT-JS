010e71cc327e9f82e2ff3cf272ea4518beb3543b
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-21 19:48:19 +0530

    ...

---
+++ React-Progs/project/template/src/Pages/SignUp.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+
+function SignUp() {
+    return (
+        <div>
+            <div className="container" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: 100 }}>
+                <div className="row " >
+                    <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                        <div className="panel-body">
+                            <form role="form" style={{ width: "300px", marginTop:20,display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                                <h5>Enter Details to SignUp</h5>
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-tag" /></span>
+                                    <input type="text" className="form-control" placeholder="Your First Name " />
+                                </div>
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-tag" /></span>
+                                    <input type="email" className="form-control" placeholder="Your Email " />
+                                </div>
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-tag" /></span>
+                                    <input type="text" className="form-control" placeholder="Create UserName " />
+                                </div>
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-tag" /></span>
+                                    <input type="number" className="form-control" placeholder="Your phone " />
+                                </div>
+
+                                <div className="form-group input-group" style={{ padding: 10 }}>
+                                    <span className="input-group-addon"><i className="fa fa-lock" /></span>
+                                    <input type="password" className="form-control" placeholder="Your Password" />
+                                </div>
+                                <a href="index.html" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} className="btn btn-primary ">SignUp Now</a>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
