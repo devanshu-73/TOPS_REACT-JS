ad4ca49998c443b59deeccedd59ccb39fbfffc59
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-29 01:57:00 +0530

    ...

---
+++ My-Practice Progs/testing-app/src/SignUpTest.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SignUpTest = () => {
+    return (
+        <div className='container'>
+            <h1 className='text-center'>Sign Up Form</h1>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+
+        </div>
+    )
+}
+
+export default SignUpTest
