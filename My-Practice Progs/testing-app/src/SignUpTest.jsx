c66f0c46bde9cc01ffde20dc0cc66a1081268cfd
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-30 16:47:05 +0530

    ...

--- My-Practice Progs/testing-app/src/SignUpTest.jsx
+++ My-Practice Progs/testing-app/src/SignUpTest.jsx
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+/* eslint-disable eqeqeq */
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const SignUpTest = () => {
-    const [data, setData] = useState();
-    useEffect(() => {
-        fetchData();
-    }, []);
-    const fetchData = async () => {
-        const res = await axios.post(`http://localhost:3000/user`)
-        setData(res.data);
-        // console.log(res.data);
+    const [data, setData] = useState({
+        username: "",
+        password: ""
+    })
+    const onchange = (e) => {
+        setData({ ...data, id: new Date().getTime().toString(), [e.target.name]: e.target.value })
+        console.log(data);
+    }
+    function validation() {
+        var result = true;
+        if (data.username == "") {
+            alert("Username is empty");
+            result = false;
+        }
+        if (data.password == "") {
+            alert("Password is empty");
+            result = false;
+        }
+        return result;
+    }
+    const onsubmit = async (e) => {
+        e.preventDefault();
+        if (validation()) {
+            const res = await axios.post('http://localhost:3000/user', data);
+            if (res.status == 201) {
+                alert("success");
+                setData({ ...data, username: "", password: "" });
+            }
+        }
     }
     return (
         <div className='container'>
-            <h1 className='text-center'>Sign Up Form</h1>
-            <form>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputName" className="form-label">UserName</label>
-                    <input type="text" value={data.name} className="form-control" />
+            <h1 className='text-center'>Sign Up</h1>
+            <hr />
+            <form className='d-flex flex-column justify-content-center align-items-center'>
+                <div>
+                    <label htmlFor="username">UserName</label>
+                    <input type="text" name="username" className="form-control" value={data.username} onChange={onchange} />
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" value={data.email} className="form-control" aria-describedby="emailHelp" />
+                <div>
+                    <label htmlFor="password">password</label>
+                    <input type="text" name="password" className="form-control" value={data.password} onChange={onchange} />
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" value={data.password} className="form-control" />
+                <div className='mt-2 d-flex justify-content-center align-items-center'>
+                    <button type="submit" onClick={onsubmit} className='btn btn-primary'>SignUp</button>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
-
         </div>
     )
 }
