9fdc511a3d94512452a2c53140a0a1fe22fefce5
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-09-30 15:10:37 +0530

    ...

--- React-Progs/project/admin-panel-1/src/App.js
+++ React-Progs/project/admin-panel-1/src/App.js
@@ -13,22 +13,23 @@ import ManageProduct from './Page/ManageProduct';
 import ManageContact from './Page/ManageContact';
 import ManageBlog from './Page/ManageBlog';
 import Error from './Page/Error';
-
+import EditUserData from './Page/Edit_userdata';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<><Login/></>}></Route>
-        <Route path="/dashboard" element={<><Header/> <Dashboard/> <Footer/> </>}></Route>
-        <Route path="/addProduct" element={<><Header/> <AddProduct/> <Footer/></>}></Route>
-        <Route path="/addBlog" element={<><Header/> <AddBlog/> <Footer/></>}></Route>
-        <Route path="/manageUser" element={<><Header/> <ManageUser/> <Footer/></>}></Route>
-        <Route path="/manageOrder" element={<><Header/> <ManageOrder/> <Footer/></>}></Route>
-        <Route path="/logout" element={<><Header/> <Logout/> <Footer/></>}></Route>
-        <Route path="/manageProduct" element={<><Header/> <ManageProduct/> <Footer/></>}></Route>
-        <Route path="/manageBlog" element={<><Header/> <ManageBlog/> <Footer/></>}></Route>
-        <Route path="/manageContact" element={<><Header/> <ManageContact/> <Footer/></>}></Route>
-        <Route path="/error" element={<><Error/></>}></Route>
+        <Route path="/" element={<><Login /></>}></Route>
+        <Route path="/dashboard" element={<><Header /> <Dashboard /> <Footer /> </>}></Route>
+        <Route path="/addProduct" element={<><Header /> <AddProduct /> <Footer /></>}></Route>
+        <Route path="/addBlog" element={<><Header /> <AddBlog /> <Footer /></>}></Route>
+        <Route path="/manageUser" element={<><Header /> <ManageUser /> <Footer /></>}></Route>
+        <Route path="/edit_user/:id" element={<><Header /> <EditUserData /></>}></Route>
+        <Route path="/manageOrder" element={<><Header /> <ManageOrder /> <Footer /></>}></Route>
+        <Route path="/logout" element={<><Header /> <Logout /> <Footer /></>}></Route>
+        <Route path="/manageProduct" element={<><Header /> <ManageProduct /> <Footer /></>}></Route>
+        <Route path="/manageBlog" element={<><Header /> <ManageBlog /> <Footer /></>}></Route>
+        <Route path="/manageContact" element={<><Header /> <ManageContact /> <Footer /></>}></Route>
+        <Route path="/error" element={<><Error /></>}></Route>
       </Routes>
     </BrowserRouter>
   )
