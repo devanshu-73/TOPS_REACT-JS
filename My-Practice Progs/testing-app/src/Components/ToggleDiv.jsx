291f1365bf20ec58baa47c38c28c7980787c4b62
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-03 19:38:15 +0530

    new....

---
+++ My-Practice Progs/testing-app/src/Components/ToggleDiv.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+const ToggleDiv = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
+    return (
+        <>
+            <div>
+                <button onClick={toggleVisibility}>Toggle Div</button>
+                {isVisible && <div style={{border:'1px solid black'}}>This div is visible.</div>}
+            </div>
+        </>
+    )
+}
+
+export default ToggleDiv
