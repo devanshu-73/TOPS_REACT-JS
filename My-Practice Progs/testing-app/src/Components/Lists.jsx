979ad350554f4f31011893ff4e9313b3bf355a03
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-03 19:10:53 +0530

    new...

--- My-Practice Progs/testing-app/src/Components/Lists.jsx
+++ My-Practice Progs/testing-app/src/Components/Lists.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Lists = () => {
     let items = [
         "DEVANSHU",
-        "chauhan"
+        "chauhan",
+        "Yash"
     ];
     // items = [];  
     return (
