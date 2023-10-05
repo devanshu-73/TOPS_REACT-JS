bd5ad1f28e930af2ed4169362060345b9df1a0be
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-05 19:31:21 +0530

    new...

--- React-Progs/project/template/src/Pages/Home.jsx
+++ React-Progs/project/template/src/Pages/Home.jsx
@@ -218,7 +218,7 @@ export default function Home() {
 
                                                 }
                                             </div >
-                                            <p className="text-body mb-3">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
+                                            <p className="text-body mb-3">Amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
                                             <div className="d-flex justify-content-between">
                                                 <a className="btn btn-sm btn-primary rounded py-2 px-4" href>View Detail</a>
                                                 <a className="btn btn-sm btn-dark rounded py-2 px-4" href>Book Now</a>
