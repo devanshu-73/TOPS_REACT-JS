c22782fdbbf47cf45d1dcfbb18ecf874ac641f04
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-13 18:50:26 +0530

    ..

--- React-Progs/project/template/src/Comps/Header2.jsx
+++ React-Progs/project/template/src/Comps/Header2.jsx
@@ -11,7 +11,7 @@ export default function Header2() {
                     <div className="row gx-0">
                         <div className="col-lg-3 bg-dark d-none d-lg-block">
                             <NavLink to="/" className="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
-                                <h1 className="m-0 text-primary text-uppercase">Hotelier</h1>
+                                <h1 className="m-0 text-primary text-uppercase">Dev</h1>
                             </NavLink>
                         </div>
                         <div className="col-lg-9">
