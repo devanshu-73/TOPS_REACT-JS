d9302ea4b4e4d3fdb5d93e2583f13333385fbc34
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-21 20:04:52 +0530

    ....

--- React-Progs/mui/src/Func_Life/Func_Img.jsx
+++ React-Progs/mui/src/Func_Life/Func_Img.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Func_Img = () => {
+
+  useEffect(() => {
+    console.log('Comp didMount'); // didMount
+
+    return (() => { console.log('Comp didUnmount') }); // didUnmount})
+
+  }, []);
+  
   return (
     <div>
-      
+      <h1>Hi I Am Func Comp</h1>
     </div>
   )
 }
--- React-Progs/mui/src/Func_Life/Func_Life.jsx
+++ React-Progs/mui/src/Func_Life/Func_Life.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+/* eslint-disable react/jsx-pascal-case */
+import React, { useEffect, useState } from 'react'
+import Func_img from './Func_img';
 
 const Func_Life = () => {
+
+  const [isImage, setisImage] = useState(true);
+  const [number, setnumber] = useState(0);
+
+  useEffect(() => {
+    console.log('didmount/update');
+  }, [number]);
+
   return (
-    <div>
-      
+    <div className='container mt-5'>
+      <button onClick={() => setnumber(number + 1)}>+</button>
+      <h1>{number}</h1>
+      <button onClick={() => setnumber(number - 1)}>-</button>
+      <hr />
+      <button onClick={() => setisImage(false)}>Hide</button>
+      <button onClick={() => setisImage(true)}>Show</button>
+      {
+        isImage ? <Func_img /> : null
+      }
     </div>
   )
 }
