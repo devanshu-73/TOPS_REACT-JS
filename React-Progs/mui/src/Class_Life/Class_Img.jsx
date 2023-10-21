0b2b37828677f1bd45c6689802e99a85fc3d79bd
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-21 19:56:00 +0530

    ...

--- React-Progs/mui/src/Class_Life/Class_Img.jsx
+++ React-Progs/mui/src/Class_Life/Class_Img.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 
 export default class Class_Img extends Component {
+    componentDidMount() {
+    
+        console.log('Component didMount');    
+    }
+    
+    componentWillUnmount() {
+        
+        console.log('Component WillUnmount');    
+    }
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <h1>Hello I Am Component</h1>
       </div>
     )
   }
--- React-Progs/mui/src/Class_Life/Class_Life.jsx
+++ React-Progs/mui/src/Class_Life/Class_Life.jsx
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
+/* eslint-disable react/jsx-pascal-case */
+import React, { Component } from 'react';
+import Class_Img from './Class_Img';
 
 export default class Class_Life extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "Raj nagar",
+            isImage: true
+        }
+    }
+
+    componentDidUpdate() {
+
+        console.log('Component DidUpdate');
+    }
     render() {
         return (
-            <div>
-
+            <div className='container mt-5'>
+                <button onClick={() => this.setState({ isImage: false })}>Hide</button>
+                <button onClick={() => this.setState({ isImage: true })}>Show</button>
+                {
+                    this.state.isImage ? <Class_Img /> : null
+                }
             </div>
         )
     }
