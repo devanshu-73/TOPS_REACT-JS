2c5e5f86bb6afdd2a4ec57946d64a477ea9edfe6
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-21 20:03:57 +0530

    ...

--- React-Progs/mui/src/App.js
+++ React-Progs/mui/src/App.js
@@ -1,34 +1,40 @@
-import Button from '@mui/material/Button';
-import StyledComp from './Styled_Comp';
-import ButtonAppBar from './ButtonAppBar';
-import ResponsiveAppBar from './ResponsiveAppBar';
+/* eslint-disable react/jsx-pascal-case */
+// import Button from '@mui/material/Button';
+// import StyledComp from './Styled_Comp';
+// import ButtonAppBar from './ButtonAppBar';
+// import ResponsiveAppBar from './ResponsiveAppBar';
+// import Class_Life from "./Class_Life/Class_Life";
+import Func_Life from './Func_Life/Func_Life';
+
 function App() {
-  let text1 = ["hello", "frew", "rv", "btrbr"];
-  let color1 = ["yellow", "white", "red", "blue", "orange"];
+  // let text1 = ["hello", "frew", "rv", "btrbr"];
+  // let color1 = ["yellow", "white", "red", "blue", "orange"];
   return (
     <>
 
-      <Button variant="contained" color="primary">Dev-1</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="outlined">Outlined</Button>
+      {/* <Button variant="contained" color="primary">Dev-1</Button> */}
+      {/* <Button variant="text">Text</Button> */}
+      {/* <Button variant="outlined">Outlined</Button> */}
 
 
       <hr />
       <hr />
 
-      {
+      {/* {
         text1.map((val, index) => (<>
           <StyledComp key={index} text={val} color_1={color1[index]} /></>
         ))
-      }
+      } */}
 
       <hr />
       <hr />
       <hr />
       <hr />
       <hr />
-      <ButtonAppBar />
-      <ResponsiveAppBar />
+      {/* <ButtonAppBar /> */}
+      {/* <ResponsiveAppBar /> */}
+      {/* <Class_Life /> */}
+      <Func_Life />
     </>
   );
 }
