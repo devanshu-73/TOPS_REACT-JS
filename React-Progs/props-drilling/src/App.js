a8373e48f6d3b912190649d0e241d88a0558d1ec
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-26 13:56:43 +0530

    ...

--- React-Progs/props-drilling/src/App.js
+++ React-Progs/props-drilling/src/App.js
@@ -1,8 +1,11 @@
-import UseContext from './UseContext/UseContext';
+/* eslint-disable react/jsx-pascal-case */
+// import UseContext from './UseContext/UseContext';
+import My_useReducer from './useReducer/My_useReducer';
 function App() {
   return (
     <div className="App">
-      <UseContext />
+      {/* <UseContext /> */}
+      <My_useReducer />
     </div>
   );
 }
