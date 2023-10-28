22dcd7fe76eb5b390ac27e2f56fc28ee96187b52
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-10-29 03:07:06 +0530

    ...

---
+++ React-Progs/redux-crud-app/src/features/userReducer.js
@@ -0,0 +1,12 @@
+const objectinia={
+    alluser:[],
+    singleuser:{},
+    demo:"Devanshu"
+}
+
+const userReducer=(state=objectinia,action)=>{
+
+    return state;
+}
+
+export default userReducer;
