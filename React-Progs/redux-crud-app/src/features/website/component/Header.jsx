8e35d35746fab3ebdef050be402f3027cf4e8ed9
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-02 14:31:08 +0530

    ....

--- React-Progs/redux-crud-app/src/features/website/component/Header.jsx
+++ React-Progs/redux-crud-app/src/features/website/component/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <div>
             <div className="p-5 bg-primary text-white text-center">
-                <h1>Dev_eloper Redux_CRUD_App</h1>
+                <h1>Redux Crud</h1>
                 <p>Resize this responsive page to see the effect!</p>
             </div>
             <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
@@ -22,6 +22,9 @@ function Header() {
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/contact">Contact</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/manage_contact">Manage Contact</NavLink>
                         </li>
                     </ul>
                 </div>
