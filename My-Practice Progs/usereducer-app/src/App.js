bbcb6e5ad227af770a44c4e8fdaaccb3f98a0d15
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-03 19:06:15 +0530

    ....

--- My-Practice Progs/usereducer-app/src/App.js
+++ My-Practice Progs/usereducer-app/src/App.js
@@ -2,7 +2,6 @@ import { toast } from 'react-toastify';
 function App() {
   return (
     <>
-      <div>Hello </div>
       <button onClick={() => (toast.success("hello"))}>Click</button>
     </>
   );
