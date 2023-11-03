e8cbb71256077d6638cb85fe51f999049d727fb0
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-03 19:15:57 +0530

    ....

--- My-Practice Progs/usereducer-app/src/App.js
+++ My-Practice Progs/usereducer-app/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from "react";
 
-import { toast } from 'react-toastify';
+// import { toast } from 'react-toastify';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <button onClick={() => (toast.success("hello"))}>Click</button>
+      {/* <button onClick={() => (toast.success("hello"))}>Click</button> */}
+      <div>{count}</div>
+      <button onClick={setCount((count) => count + 1)}></button>
     </>
   );
 }
