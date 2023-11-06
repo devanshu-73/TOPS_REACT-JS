56f35446ecf61bce23e3756ef0677d8ec963da37
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-06 18:37:20 +0530

    ...

--- My-Practice Progs/usereducer-app/src/index.js
+++ My-Practice Progs/usereducer-app/src/index.js
@@ -8,8 +8,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
+
     <ToastContainer></ToastContainer>
     <App />
+
   </React.StrictMode>
 
 );
