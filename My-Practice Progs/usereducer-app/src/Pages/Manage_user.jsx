5affc80fae0a7120cdd01a45cf8a0e1795b7f924
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-09 19:18:33 +0530

    .....

--- My-Practice Progs/usereducer-app/src/Pages/Manage_user.jsx
+++ My-Practice Progs/usereducer-app/src/Pages/Manage_user.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
+
 const Manage_user = () => {
+
+  const [userData, setUserData] = useState([]);
+
+  const fetchData = async () => {
+
+    const res = await axios.get(`http://localhost:3000/users`);
+    setUserData(res.data);
+
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
     <div>
-        
+      <table className='table-bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            userData.map((user) => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   )
 }
