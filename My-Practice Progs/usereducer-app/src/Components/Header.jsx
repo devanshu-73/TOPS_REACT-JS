cf2ffa24a4f6850d74c747eba67c8701840ee0fa
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-10 20:03:21 +0530

    ....

--- My-Practice Progs/usereducer-app/src/Components/Header.jsx
+++ My-Practice Progs/usereducer-app/src/Components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      Header...
-    </div>
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0">
+      <div className="collapse navbar-collapse justify-content-evenly" id="navbarCollapse">
+        <div className="navbar-nav mr-auto py-0">
+          <NavLink className="nav-item nav-link" to="/"><h1>User</h1></NavLink>
+          <NavLink className="nav-item nav-link" to="/manageuser"><h1>Manage User Data</h1></NavLink>
+        </div>
+      </div>
+    </nav>
   )
 }
 
