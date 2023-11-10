cfc30d00ce93b010b73fb7d22fb93b1d2ba1b5b1
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-10 20:02:02 +0530

    ...

--- My-Practice Progs/usereducer-app/src/App.js
+++ My-Practice Progs/usereducer-app/src/App.js
@@ -1,14 +1,19 @@
-// import Header from './Components/Header';
-// import Footer from './Components/Footer';
-
+/* eslint-disable react/jsx-pascal-case */
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import User from "./Pages/User";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Manage_user from './Pages/Manage_user';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
-      {/* <Footer /> */}
-      <User />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<><Header /><User /><Footer /></>}></Route>
+          <Route path="/manageuser" element={<><Header /><Manage_user /><Footer /></>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
