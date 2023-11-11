efba22218ace349ebd40fb8e20123d934565b529
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-11 21:03:27 +0530

    ....

--- My-Practice Progs/usereducer-app/src/Pages/BothUser.jsx
+++ My-Practice Progs/usereducer-app/src/Pages/BothUser.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-
+import axios from 'axios';
+import React, { useState } from 'react';
+import { v4 as uuid } from 'uuid'
 const BothUser = () => {
-  return (
-    <div>
-      <form action="">
-        <div className='mb-3 mt-3'>
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" className='form-lable' />
+    const [users, setUsers] = useState({
+        id: uuid().slice(0, 5),
+        name: "",
+        email: "",
+        password: ""
+    })
+    return (
+        <div>
+            <form action="" onSubmit={onsubmit}>
+                <div className='mb-3 mt-3'>
+                    <label htmlFor="name" className='form-lable'>Name</label>
+                    <input type="text" name="name" value={users.name} onChange={(e) => (setUsers({ ...users, [e.target.name]: e.target.value }))} className='form-control' placeholder='Enter Name' />
+                </div>
+                <button type="submit" className='btn btn-danger' >Submit</button>
+            </form>
         </div>
-      </form>
-    </div>
-  )
+    )
 }
 
 export default BothUser
