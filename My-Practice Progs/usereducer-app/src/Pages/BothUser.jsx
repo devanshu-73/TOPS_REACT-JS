ff320c47494fcbd7bed54935955ae8f9631ab75e
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-11 20:25:59 +0530

    ...

---
+++ My-Practice Progs/usereducer-app/src/Pages/BothUser.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BothUser = () => {
+  return (
+    <div>
+      <form action="">
+        <div className='mb-3 mt-3'>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" className='form-lable' />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default BothUser
