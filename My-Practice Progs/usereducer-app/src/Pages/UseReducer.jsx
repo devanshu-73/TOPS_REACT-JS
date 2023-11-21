96df74c4456f1cf7333c750e9a8ad86fce9c80cf
Devanshu-73 <devanshuchauhan732000@gmail.com>
2023-11-21 16:23:41 +0530

    ....

--- My-Practice Progs/usereducer-app/src/Pages/UseReducer.jsx
+++ My-Practice Progs/usereducer-app/src/Pages/UseReducer.jsx
@@ -1,8 +1,15 @@
 import React, { useReducer } from 'react'
 
 const initState = 0;
-const reducer = (state) => {
-
+const reducer = (state, action) => {
+    switch (action) {
+        case "increment":
+            return state += 1;
+        case "decrement":
+            return state -= 1;
+        default:
+            return state;
+    }
 }
 
 const UseReducer = () => {
