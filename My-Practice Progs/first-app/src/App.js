59b9e5eac43e24f091f39862e0fc1a9cf32b2758
Devanshu-73 <devanshuchauhan732000@gmail.com>
2024-02-25 00:00:28 +0530

    ...

--- My-Practice Progs/first-app/src/App.js
+++ My-Practice Progs/first-app/src/App.js
@@ -1,11 +1,13 @@
 // import NavScrollExample from "./MainNavbar";
-import TextExample from "./CardMain";
+// import TextExample from "./CardMain";
 function App() {
   return (
     <>
+
+    <h1>hello</h1>
       {/* <NavScrollExample /> */}
       {/* <div className="d-flex"> */}
-        <TextExample />
+        {/* <TextExample /> */}
         {/* <TextExample /> */}
         {/* <TextExample /> */}
         {/* <TextExample /> */}
